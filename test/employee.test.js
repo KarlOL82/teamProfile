5ba72ba3608970464e46aa46df11bc0d3cf62018
KarLOL82 <klinfeldt@gmail.com>
2022-08-21 20:44:05 -0700

    testing files almost complete

--- test/employee.test.js
+++ test/employee.test.js
@@ -5,39 +5,43 @@ describe("Employee", () => {
         it("should create an object with 'name' string, 'id' string, 'email' string, and 'role' string ", () => {
           const employee =  new Employee();
 
-          expect(employee).toEqual({name: "", id: "", email: "", role: ""})
+          expect(typeof(employee)).toBe("object");
         });
     });
     describe("getName", () => {
         it("should return a 'name' string", () => {
-            const name = Employee.name;
+            const name = "Foo";
+            const employee = new Employee(name, 1, "foo@bar.com", "Employee")
 
-            expect(name).toEqual(getName(answers));
+            expect(employee.getName()).toEqual(name);
 
         });
     });
     describe("getId", () => {
         it("should return a 'id' string", () => {
-            const id = Employee.id;
+            const id = "1";
+            const employee = new Employee("Foo", id, "foo@bar.com", "Employee")
 
-            expect(id).toEqual(getId(answers));
+            expect(employee.getId()).toEqual(id);
 
         });
     });
     describe("getEmail", () => {
-        it("should return a 'id' string", () => {
-            const id = Employee.id;
+        it("should return a 'email' string", () => {
+            const email = "foo@bar.com";
+            const employee = new Employee("Foo", 1, email, "Employee")
 
-            expect(email).toEqual(getEmail(answers));
+            expect(employee.getEmail()).toEqual(email);
 
         });
     });
     describe("getRole", () => {
         it("should return a 'role' string", () => {
             const role = "Employee";
+            const employee = new Employee("Foo", 1, "foo@bar.com", role)
 
-            expect(role).toEqual(getRole(answers));
-
-        });
+            expect(employee.getRole()).toEqual(role);
     });            
 });
+
+})
